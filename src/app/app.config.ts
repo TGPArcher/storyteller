82582e2d143745d2fa68ef2b25707b21f085368e
Petru Grozea <petru.grozea@softelligence.net>
2023-10-12 18:17:42 +0300

    Add test routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PageThreeComponent } from './page-three/page-three.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    provideRouter([
+      {
+        path: 'page-one',
+        component: PageOneComponent,
+      },
+      {
+        path: 'page-two',
+        component: PageTwoComponent,
+      },
+      {
+        path: 'page-three',
+        component: PageThreeComponent,
+      },
+    ]),
+  ],
 };
